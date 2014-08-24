d81d0fcf06971dab703bd5690d0c4d0061525ce7
Jason Frame <jason@onehackoranother.com>
2014-08-25 00:02:30 +0100

    tabs -> spaces

--- cli.js
+++ cli.js
@@ -4,57 +4,57 @@ var spawn = require('child_process').spawn;
 var commands, procs;
 
 try {
-	
-	commands = fs
-		.readFileSync('spin.up', {encoding: 'utf8'})
-		.split(/\n+/)
-		.map(function(l) { return l.replace(/#[^$]*$/, ''); })
-		.map(function(l) { return l.trim(); })
-		.filter(function(l) { return l.length > 0; });
+    
+    commands = fs
+        .readFileSync('spin.up', {encoding: 'utf8'})
+        .split(/\n+/)
+        .map(function(l) { return l.replace(/#[^$]*$/, ''); })
+        .map(function(l) { return l.trim(); })
+        .filter(function(l) { return l.length > 0; });
 
 } catch (e) {
 
-	process.stderr.write("no spin.up file found!");
-	process.exit(1);
+    process.stderr.write("no spin.up file found!");
+    process.exit(1);
 
 }
 
 var nextColor = 0;
 function makeColor() {
-	// cycles green, yellow, blue, magenta, cyan
-	return ((nextColor++) % 5) + 2;
+    // cycles green, yellow, blue, magenta, cyan
+    return ((nextColor++) % 5) + 2;
 }
 
 procs = commands.map(function(c, taskIx) {
 
-	var args 	= c.split(/\s+/);
-	var cmd  	= args.shift();
-	var color 	= makeColor();
-	var child 	= spawn(cmd, args);
-	var prefix 	= "[" + taskIx + ":" + child.pid + "]";
+    var args    = c.split(/\s+/);
+    var cmd     = args.shift();
+    var color   = makeColor();
+    var child   = spawn(cmd, args);
+    var prefix  = "[" + taskIx + ":" + child.pid + "]";
 
-	function _colorize(text) {
-		return "\x1b[3" + color + "m" + text + "\x1b[0m";
-	}
+    function _colorize(text) {
+        return "\x1b[3" + color + "m" + text + "\x1b[0m";
+    }
 
-	process.stderr.write(_colorize(prefix + " " + c) + "\n");
+    process.stderr.write(_colorize(prefix + " " + c) + "\n");
 
-	child.stdout.setEncoding('utf8');
-	child.stdout.on('data', function(str) {
-		process.stdout.write(_colorize(prefix + " " + str));
-	});
+    child.stdout.setEncoding('utf8');
+    child.stdout.on('data', function(str) {
+        process.stdout.write(_colorize(prefix + " " + str));
+    });
 
-	child.stderr.setEncoding('utf8');
-	child.stderr.on('data', function(str) {
-		process.stderr.write(_colorize(prefix + " " + str));
-	});
+    child.stderr.setEncoding('utf8');
+    child.stderr.on('data', function(str) {
+        process.stderr.write(_colorize(prefix + " " + str));
+    });
 
-	child.on('exit', function() {
-		process.stderr.write(prefix + " terminated\n");
-	});
+    child.on('exit', function() {
+        process.stderr.write(prefix + " terminated\n");
+    });
 
-	child.on('error', function(err) {
-		console.log(err);
-	});
+    child.on('error', function(err) {
+        console.log(err);
+    });
 
 });
